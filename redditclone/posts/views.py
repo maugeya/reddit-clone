b761b82b85cb79a7ea354641f9f88d6f8e33d4a9
Angela Maugey <a.maugey@gmail.com>
2017-09-01 10:12:07 +0100

    posts adding in database

--- redditclone/posts/views.py
+++ redditclone/posts/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from . import models
 
 # Create your views here.
 @login_required
 def create(request):
-    return render(request, 'posts/create.html')
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['url']:
+            post = models.Post()
+            post.title = request.POST['title']
+            post.url = request.POST['url']
+            post.pub_date = timezone.datetime.now()
+            post.author = request.user
+            post.save()
+    else:
+        return render(request, 'posts/create.html')
