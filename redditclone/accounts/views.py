2fcc0b850f7ddce0583065fe6422c683091b6676
Angela Maugey <a.maugey@gmail.com>
2017-09-01 09:11:31 +0100

    making changes to redirects after the login

--- redditclone/accounts/views.py
+++ redditclone/accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 
@@ -29,6 +29,8 @@ def loginview(request):
 
         if user is not None:
             login(request, user)
+            if 'next' in request.POST:
+                return redirect(request.POST['next'])
             return render(request, 'accounts/login.html', {'error': 'Login SUCCESS!!'})
         else:
             return render(request, 'accounts/login.html', {'error': 'Login credentials unknown'})
