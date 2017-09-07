413f122fec2ca9a32c93e6a3ac62a0843d6e48d8
Angela Maugey <a.maugey@gmail.com>
2017-09-07 14:12:01 +0100

    up and down vote buttons now working

--- redditclone/posts/views.py
+++ redditclone/posts/views.py
@@ -1,24 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
-from . import models
+from .models import Post
 
 # Create your views here.
 @login_required
 def create(request):
     if request.method == 'POST':
         if request.POST['title'] and request.POST['url']:
-            post = models.Post()
+            post = Post()
             post.title = request.POST['title']
-            post.url = request.POST['url']
+            if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
+                post.url = request.POST['url']
+            else:
+                post.url = 'http://' + request.POST['url']
             post.pub_date = timezone.datetime.now()
             post.author = request.user
             post.save()
-            return render(request, 'posts/home.html')
+            return redirect('home')
         else:
             return render(request, 'posts/create.html', {'error':'Title and URL are required!'})
     else:
         return render(request, 'posts/create.html')
 
 def home(request):
-    return render(request, 'posts/home.html')
+    posts = Post.objects.order_by('votes_total')
+    return render(request, 'posts/home.html', {'posts':posts})
+
+def upvote(request, pk):
+    if request.method == 'POST':
+        post = Post.objects.get(pk=pk)
+        post.votes_total += 1
+        post.save()
+        return redirect('home')
+
+def downvote(request, pk):
+    if request.method == 'POST':
+        post = Post.objects.get(pk=pk)
+        post.votes_total -= 1
+        post.save()
+        return redirect('home')
